2a52c601cd57c080c4b1fbe89ca3df135c64c3f4
Albert E. Hidalgo Taveras <itsalb3rt@gmail.com>
2019-11-28 14:57:12 -0400

    now the login work and notify when user type wrong credentials

--- src/views/Auth/Login.vue
+++ src/views/Auth/Login.vue
@@ -8,15 +8,19 @@
                 <v-row>
                     <v-col cols="12">
                         <v-text-field
-                            label="Username"
+                            v-model="email"
+                            label="Email"
                             placeholder="jhondoe@domain.com"
                             outlined
+                            :rules="rules.email"
                         ></v-text-field>
                         <v-text-field
+                            v-model="password"
                             label="Password"
                             type="password"
                             placeholder="*********"
                             outlined
+                            :rules="rules.requiredField"
                         ></v-text-field>
                     </v-col>
                     <v-col cols="12">
@@ -34,18 +38,47 @@
     </div>
 </template>
 <script>
+import formRules from '@/mixins/Miscellany/FormRules';
+
 export default {
+    mixins: [formRules],
     data() {
         return {
-            loading: false
+            loading: false,
+            email: '',
+            password: ''
         };
     },
     methods: {
         login() {
             this.loading = true;
-            setTimeout(() => {
-                this.loading = false;
-            }, 2000);
+            const payload = { email: this.email, password: this.password };
+            this.$store
+                .dispatch('auth/login', payload)
+                .then(response => {
+                    setTimeout(() => {
+                        window.localStorage.setItem(
+                            'token',
+                            response.data.token
+                        );
+                        window.localStorage.setItem(
+                            'employee',
+                            JSON.stringify(response.data)
+                        );
+                        this.$store.commit('auth/SET_EMPLOYEE', response.data);
+                        this.$router.push('/tickets/list');
+                    }, 1000);
+                })
+                .catch(error => {
+                    console.log(error);
+                    this.loading = false;
+                    this.$store.commit('snackbar/setSnackbar', {
+                        show: true,
+                        message: 'Wrong credentials',
+                        color: 'error',
+                        top: true
+                    });
+                });
         }
     }
 };
