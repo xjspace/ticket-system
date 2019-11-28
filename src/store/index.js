f354cb44f5b31a96dd5cb2f7cc4c9efa080b9d6f
Albert E. Hidalgo Taveras <itsalb3rt@gmail.com>
2019-11-28 17:14:11 -0400

    added tickets module

--- src/store/index.js
+++ src/store/index.js
@@ -2,12 +2,16 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import auth from './modules/Auth'
 import snackbar from './modules/Snackbar'
+import tickets from './modules/Tickets'
+import employees from './modules/Employees'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   modules: {
     auth,
-    snackbar
+    snackbar,
+    tickets,
+    employees
   }
 })
