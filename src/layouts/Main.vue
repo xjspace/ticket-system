e17bb78123965ceb64a6c63fad9c80d7371c23b7
Albert E. Hidalgo Taveras <itsalb3rt@gmail.com>
2019-11-28 11:33:45 -0400

    create a main layout when user is authenticate

---
+++ src/layouts/Main.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <user-menu />
+        <v-container>
+            <router-view />
+        </v-container>
+    </div>
+</template>
+<script>
+import userMenu from "@/components/Interface/Menu";
+
+export default {
+    components: { userMenu }
+};
+</script>
