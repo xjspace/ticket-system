0f630b6cb7f63a3303c7dfaef27b938fd7420db7
Albert E. Hidalgo Taveras <itsalb3rt@gmail.com>
2019-11-28 17:14:38 -0400

    added tickets views

---
+++ src/views/Tickets/List.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <v-card>
+            <v-card-text>
+                <v-row>
+                    <v-col cols="12">
+                        <v-btn
+                            class="float-right"
+                            color="primary"
+                            to="/tickets/create"
+                            >Create Ticket</v-btn
+                        >
+                    </v-col>
+                    <v-col cols="12">
+                        <v-data-table
+                            :headers="headers"
+                            :items="tickets"
+                            :items-per-page="5"
+                        ></v-data-table
+                    ></v-col>
+                </v-row>
+            </v-card-text>
+        </v-card>
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            headers: [
+                { text: '#', align: 'left', value: 'id_ticket' },
+                { text: 'Description', value: 'description' },
+                { text: 'Employee(s)', value: 'employees' },
+                { text: 'Date', value: 'create_at' },
+                { text: 'Status', value: 'status' },
+                { text: 'Actions', value: 'actions' }
+            ],
+            tickets: []
+        };
+    }
+};
+</script>
