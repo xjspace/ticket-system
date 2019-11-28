c85c1de6ca8a4f4af26373e66969697523848e4f
Albert E. Hidalgo Taveras <itsalb3rt@gmail.com>
2019-11-28 11:32:06 -0400

    create a componentes for create employees and menu interface

---
+++ src/components/Employees/CreateDialog.vue
@@ -0,0 +1,109 @@
+<template>
+    <div>
+        <v-row justify="center">
+            <v-dialog v-model="showDialog" persistent max-width="500">
+                <v-card>
+                    <v-card-title class="headline">
+                        Create employee
+                    </v-card-title>
+                    <v-card-text>
+                        <v-row>
+                            <v-col cols="12">
+                                <v-form ref="createEmployeeForm">
+                                    <v-text-field
+                                        v-model="employee.firstName"
+                                        outlined
+                                        name="first_name"
+                                        label="First Name"
+                                        id="first_name"
+                                        :rules="rules.requiredInput"
+                                    ></v-text-field>
+                                    <v-text-field
+                                        v-model="employee.lastName"
+                                        :rules="rules.requiredInput"
+                                        outlined
+                                        name="last_name"
+                                        label="Last Name"
+                                        id="last_name"
+                                    ></v-text-field>
+                                    <v-text-field
+                                        v-model="employee.email"
+                                        :rules="rules.email"
+                                        outlined
+                                        type="email"
+                                        name="email"
+                                        label="Email"
+                                        id="email"
+                                    ></v-text-field>
+                                    <v-select
+                                        v-model="employee.status"
+                                        :rules="rules.requiredInput"
+                                        outlined
+                                        :items="status"
+                                        label="Status"
+                                    ></v-select>
+                                    <v-text-field
+                                        v-model="employee.password"
+                                        :rules="rules.minLength8"
+                                        outlined
+                                        type="password"
+                                        name="password"
+                                        label="Password"
+                                        id="password"
+                                    ></v-text-field>
+                                    <v-text-field
+                                        v-model="employee.confirmPassword"
+                                        :rules="rules.minLength8"
+                                        outlined
+                                        type="password"
+                                        name="password2"
+                                        label="Confirm password"
+                                        id="password2"
+                                    ></v-text-field>
+                                </v-form>
+                            </v-col>
+                        </v-row>
+                    </v-card-text>
+                    <v-card-actions>
+                        <v-btn color="success" :loading="loading" @click="create()">
+                            Save
+                        </v-btn>
+                        <v-spacer></v-spacer>
+                        <v-btn color="error" :loading="loading" @click="$emit('cancel-creation')">
+                            Cancel
+                        </v-btn>
+                    </v-card-actions>
+                </v-card>
+            </v-dialog>
+        </v-row>
+    </div>
+</template>
+<script>
+import FormRules from '@/mixins/Miscellany/FormRules';
+
+export default {
+    props: ['showDialog'],
+    mixins: [FormRules],
+    data() {
+        return {
+            status: ['active', 'inactive'],
+            employee: {
+                firstName: '',
+                lastName: '',
+                email: '',
+                status: 'active',
+                password: '',
+                confirmPassword: ''
+            },
+            loading:false
+        };
+    },
+    methods:{
+        create(){
+            if(this.$refs.createEmployeeForm.validate()){
+                this.$emit('save-creation')
+            }
+        }
+    }
+};
+</script>
---
+++ src/components/Interface/Menu.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <v-toolbar>
+            <v-toolbar-items>
+                <v-btn text>Tickets</v-btn>
+                <v-btn to="/employees/list" text>Employees</v-btn>
+                <v-btn text>Reports</v-btn>
+            </v-toolbar-items>
+            <template v-if="$vuetify.breakpoint.smAndUp">
+                <v-btn icon>
+                    <v-icon>mdi-export-variant</v-icon>
+                </v-btn>
+                <v-btn icon>
+                    <v-icon>mdi-delete-circle</v-icon>
+                </v-btn>
+                <v-btn icon>
+                    <v-icon>mdi-plus-circle</v-icon>
+                </v-btn>
+            </template>
+        </v-toolbar>
+    </div>
+</template>
+<script>
+export default {};
+</script>
