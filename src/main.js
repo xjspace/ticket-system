9580a2619d16fe7298701148b414dabf51ade9d6
Albert E. Hidalgo Taveras <itsalb3rt@gmail.com>
2019-11-27 23:01:13 -0400

    added axios

--- src/main.js
+++ src/main.js
@@ -6,8 +6,11 @@ import store from './store'
 import vuetify from './plugins/vuetify';
 import 'roboto-fontface/css/roboto/roboto-fontface.css'
 import '@fortawesome/fontawesome-free/css/all.css'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 
 Vue.config.productionTip = false
+Vue.use(VueAxios, axios)
 
 new Vue({
   router,
