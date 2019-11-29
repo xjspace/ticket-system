84cc884eac3e091b4092a13c3b0ad7b0246ffa58
Albert E. Hidalgo Taveras <itsalb3rt@gmail.com>
2019-11-28 21:44:30 -0400

    ticket now can delete from the list of tickets

--- src/views/Tickets/List.vue
+++ src/views/Tickets/List.vue
@@ -18,28 +18,45 @@
                             :items-per-page="5"
                         >
                             <template v-slot:item.actions="{ item }">
-                                <v-btn :to="`/tickets/view/${item.id_ticket}`" text small color="secondary">View</v-btn>
+                                <v-btn
+                                    :to="`/tickets/view/${item.id_ticket}`"
+                                    text
+                                    small
+                                    color="secondary"
+                                    >View</v-btn
+                                >
                                 <v-btn text small color="secondary">Edit</v-btn>
-                                <v-btn text small color="error">Delete</v-btn>
+                                <v-btn
+                                    text
+                                    small
+                                    color="error"
+                                    @click="toggleDeleteDialog(item)"
+                                    >Delete</v-btn
+                                >
                             </template>
                         </v-data-table></v-col
                     >
                 </v-row>
             </v-card-text>
         </v-card>
+        <delete-dialog
+            title="Delete"
+            message="Are you sure you want to delete this ticket?"
+            buttonLabelConfirm="Delete"
+            buttonLabelCancel="No, keep it"
+            :show-dialog="showDeleteDialog"
+            @confirm-delete="deleteTicket()"
+            @cancel-delete="showDeleteDialog = false"
+        />
     </div>
 </template>
 <script>
+import deleteDialog from '@/components/Interface/DeleteDialog';
+
 export default {
+    components: { deleteDialog },
     mounted() {
-        this.$store
-            .dispatch('tickets/get')
-            .then(response => {
-                this.$store.commit('tickets/SET_TICKETS', response.data);
-            })
-            .catch(error => {
-                console.log(error);
-            });
+        this.requestTickets();
     },
     data() {
         return {
@@ -50,8 +67,37 @@ export default {
                 { text: 'Date', value: 'create_at' },
                 { text: 'Status', value: 'status' },
                 { text: 'Actions', value: 'actions' }
-            ]
+            ],
+            showDeleteDialog: false,
+            tempTicketToDelete: {}
         };
+    },
+    methods: {
+        requestTickets() {
+            this.$store
+                .dispatch('tickets/get')
+                .then(response => {
+                    this.$store.commit('tickets/SET_TICKETS', response.data);
+                })
+                .catch(error => {
+                    console.log(error);
+                });
+        },
+        toggleDeleteDialog(ticket) {
+            this.tempTicketToDelete = ticket;
+            this.showDeleteDialog = true;
+        },
+        deleteTicket() {
+            this.$store
+                .dispatch('tickets/delete', this.tempTicketToDelete.id_ticket)
+                .then(response => {
+                    this.requestTickets();
+                    this.showDeleteDialog = false;
+                })
+                .catch(error => {
+                    console.log(error);
+                });
+        }
     }
 };
 </script>
