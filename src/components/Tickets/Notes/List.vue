6a50bd487331e77106bd2e7dafde49e6be8b478a
Albert E. Hidalgo Taveras <itsalb3rt@gmail.com>
2019-11-29 21:50:23 -0400

    create a ticket note components

---
+++ src/components/Tickets/Notes/List.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <v-row
+            v-for="(note, index) in $store.getters['notes/getNotes']"
+            :key="index"
+        >
+            <v-col cols="10">{{ note.full_name }} - {{ note.email }}</v-col>
+            <v-col cols="2">{{ note.create_at }}</v-col>
+            <v-col cols="12">
+                <div class="mb-4">
+                    <p class="black--text">{{ note.note }}</p>
+                </div>
+                <div>
+                    <v-divider></v-divider>
+                </div>
+            </v-col>
+        </v-row>
+    </div>
+</template>
+<script>
+export default {};
+</script>
