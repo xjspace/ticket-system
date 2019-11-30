669bd2c1a54e839e6affee6ee48804e86118cdf3
Albert E. Hidalgo Taveras <itsalb3rt@gmail.com>
2019-11-29 22:26:15 -0400

    minor modification, formatter date

--- src/components/Tickets/Notes/List.vue
+++ src/components/Tickets/Notes/List.vue
@@ -4,8 +4,12 @@
             v-for="(note, index) in $store.getters['notes/getNotes']"
             :key="index"
         >
-            <v-col cols="10">{{ note.full_name }} - {{ note.email }}</v-col>
-            <v-col cols="2">{{ note.create_at }}</v-col>
+            <v-col cols="9">{{ note.full_name }} - {{ note.email }}</v-col>
+            <v-col cols="3">
+                <p class="float-right">
+                    {{ parseDate(note.create_at, 'DD/MM/YYYY h:mm:ss A') }}
+                </p>
+            </v-col>
             <v-col cols="12">
                 <div class="mb-4">
                     <p class="black--text">{{ note.note }}</p>
@@ -18,5 +22,12 @@
     </div>
 </template>
 <script>
-export default {};
+import moment from 'moment';
+export default {
+    methods: {
+        parseDate(date, format) {
+            return moment(date).format(format);
+        }
+    }
+};
 </script>
